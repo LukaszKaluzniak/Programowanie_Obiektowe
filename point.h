7e8df2811272a84ddd2d9c71a83647e4be837ff3
Łukasz Kaluźniak <35638803+LukaszKaluzniak@users.noreply.github.com>
2018-03-04 15:36:16 +0100

    Add files via upload

---
+++ point.h
@@ -0,0 +1,14 @@
+#ifndef POINT_H
+#define POINT_H
+//-----
+class Point
+{
+    float x, y;
+
+public:
+    Point(float x, float y);
+    float getX();
+    float getY();
+};
+//-----
+#endif // POINT_H
