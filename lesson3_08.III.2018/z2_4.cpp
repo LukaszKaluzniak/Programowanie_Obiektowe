887d476e838e577cbd98577b990b0a45ad8b776d
Łukasz Kaluźniak <35638803+LukaszKaluzniak@users.noreply.github.com>
2018-03-12 08:20:00 +0100

    Add files via upload

---
+++ lesson3_08.III.2018/z2_4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string s, lf;
+    stringstream sstream;
+    ifstream f;
+    unsigned int c = 0;
+    size_t p = 0;
+
+    f.open("file.txt");
+    sstream << f.rdbuf();
+    s = sstream.str();
+    f.close();
+    cin >> lf;
+
+    while(true)
+    {
+        p = s.find(lf, p);
+        if(p == string::npos)
+            break;
+        c++;
+        p += lf.length();
+    }
+    cout << c << endl;
+
+    return 0;
+}
