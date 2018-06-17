a1fc5fa4565bb65ee8e71c1f9852261b43f9b9c2
Łukasz Kaluźniak <35638803+LukaszKaluzniak@users.noreply.github.com>
2018-06-17 19:32:41 +0200

    Update main.cpp

--- lesson12_24.V.2018/z1/main.cpp
+++ lesson12_24.V.2018/z1/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "array.h"
 
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  Array<int> arr(10);
+  
+  for(int i=0; i<10; i++)
+    cout << arr[i] << endl;
+  
+  return 0;
+}
