90f08d13d2e45543b8554db69e30d6eb07b4ed45
Łukasz Kaluźniak <35638803+LukaszKaluzniak@users.noreply.github.com>
2018-06-21 14:29:33 +0200

    Create four.cpp

---
+++ lesson15_21.VI.2018/four.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Klasa
+{
+private:
+    static int nextId;
+    int Id;
+public:
+    Klasa() : Id(++nextId)
+    {
+        cout << "Nowy obiekt o id " << Id << endl;
+    }
+    ~Klasa()
+    {
+        cout << "Destruktor obiektu o id " << Id << endl;
+    }
+
+    int test(int arg)
+    {
+        cout << "Funkcja testowa obiektu o id " << Id << endl;
+        return arg + 4;
+    }
+    int getId()
+    {
+        return Id;
+    }
+};
+
+int Klasa::nextId = 0;
+
+/*
+void ftestowa(shared_ptr<Klasa> ptrf)
+{
+    cout<<ptrf->test(2)<<endl;
+}
+*/
+
+int main(int argc, char *argv[])
+{
+    shared_ptr<Klasa> ptr1(new Klasa);
+    shared_ptr<Klasa> ptr2(ptr1);
+    shared_ptr<Klasa> ptr3(ptr1);
+    cout<<ptr1->test(3)<<endl;
+    cout<<ptr2->test(8)<<endl;
+    cout<<ptr3->test(2)<<endl;
+    cout << "KONIEC" << endl;
+    return 0;
+}
